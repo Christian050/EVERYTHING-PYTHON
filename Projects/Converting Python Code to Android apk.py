34595a369a84aaa25155da445a6afa83fe35cf73
Christian050 <cristiancolley@gmail.com>
2023-03-03 16:34:01 +0000

    03/03/2023

---
+++ Projects/Converting Python Code to Android apk.py
@@ -0,0 +1,47 @@
+# Import MDApp from kivymd.app, kivymd.uix.label import MDLabel
+
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+class MainApp(MDApp):
+    def build(self):
+        return MDLabel(text='Using Kivy to convert python code to android apk',
+                       halign='center')
+
+
+if __name__ == '__main__':
+    MainApp().run()
+
+'''
+In Google collab create a new notebook
+
+Write code in 'Kivy.txt' one by one in the notebook(code)
+
+NB: Add new code line 
+
+Before running '! buildozer init' upload this file to the notebook
+
+Type 'Y' to continue
+
+Source code will be located in  sample_data/buildozer.spec
+
+Title/ Name of application will be located on line 4
+Package name on line 52
+
+Icon filename can be customizable on line 52
+
+
+On line 39
+edit requirements to make it look like:
+
+    requirements = python3, kivy==2.0.0, kivymd, pillow
+    
+
+Run next code in 'Kivy.txt' in notebook(code) and type 'Y' to continue.
+
+The apk file will be located in the bin directory,
+download file and save to cloud
+
+Finally, open cloud on phone and install apk file.
+'''
